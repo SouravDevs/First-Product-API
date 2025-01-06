2b5f7d3b5252ca7f82b4a0bdc2b7e710eaf2869b
SouravDevs <sourav2x4x@gmail.com>
2025-01-06 20:35:04 +0530

    h

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ import Product from "./Models/product.model.js";
 const app = express();
 const PORT = 5000;
 
-app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // or '*'
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hi from Node.js");
